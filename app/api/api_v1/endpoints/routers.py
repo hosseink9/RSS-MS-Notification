d19644dfd798e31356e8cc3a594590f72ca5a5a0
hosseink9 <hosseinkalantari.29@gmail.com>
2023-11-04 23:40:41 +0330

    Create generate otp

--- app/api/api_v1/endpoints/routers.py
+++ app/api/api_v1/endpoints/routers.py
@@ -7,3 +7,8 @@ from ....utils.utils import create_otp, verify_otp
 router = APIRouter(tags=["notification"])
 
 
+@router.post('/create_otp', status_code=status.HTTP_201_CREATED)
+async def generate_otp(data: UserRequest):
+    await create_otp(data.id)
+    return f'otp was sent to {data.username}'
+
